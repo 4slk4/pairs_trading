6ffb0e42588380700b5e41e0a803382c40452b5a
Khang <student123@khang.com>
2022-06-26 15:37:29 -0700

    Changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pairs trading project for Summer Research Experience 2022
+Requirement:
+Python 3.10
+pandas
+numpy
+matplotlib
+statsmodels
+
+When you compile:
+g++ main.cpp -o main $(/usr/bin/python3-config --embed --ldflags)
+
+When run the 'main' file:
+./main <stock1>.csv <stock2>.csv
--- main.cpp
+++ main.cpp
@@ -11,34 +11,58 @@
 std::vector<double> log_return(const char* f_name);
 void vec_to_file(const std::vector<double>& dt, const char* f_name);
 
-int main()
+int main(int argc, char* argv[])
 {
-    auto neo = std::async(std::launch::async, log_return, "MSFT.csv");
-    std::vector<double> result = neo.get();
-    const char* name = "msft";
-    vec_to_file(result, name);
+    auto neo1 = std::async(std::launch::async, log_return, argv[1]);
+    auto neo2 = std::async(std::launch::async, log_return, argv[2]);
+    std::vector<double> return1 = neo1.get();
+    std::vector<double> return2 = neo2.get();
+    
+    const char* name = "log_return1.txt";
+    vec_to_file(return1, name);
 
-    double p = pearson_co(result, result);
-    printf("%.4f\n", p);
+    const char* name2 = "log_return2.txt";
+    vec_to_file(return2, name2);
+
+    double p = pearson_co(return1, return1);
+    printf("The Pearson Correlation Coefficient of 2 stocks is: %.4f\n", p);
 
     //Starting Python interpreter
-    // Py_Initialize();
-    // PyRun_SimpleString("import pandas as pd\n"
-    // "import numpy as np\n"
-    // "import matplotlib.pyplot as plt");
+    Py_Initialize();
 
-    // //Building a string command to read the log_return file
-    // const char * py_read1 = "df = pd.read_csv('";
-    // const char * py_read2 = "')\n";
-    // char buf[1024];
-    // snprintf(buf, sizeof(buf), "%s%s%s", py_read1, name, py_read2);
-    // const char* str = buf;
+    //Import libraries
+    PyRun_SimpleString("import pandas as pd\n"
+    "import numpy as np\n"
+    "import matplotlib.pyplot as plt\n"
+    "from statsmodels.tsa.stattools import adfuller");
 
-    // PyRun_SimpleString(str);
-    // //PyRun_SimpleString("df.plot(x='Date', y='Adj Close', kind='line')\n"
-    // PyRun_SimpleString("df.plot()\n" 
-    // "plt.show()\n");
-    // Py_Finalize();
+    //Calculate cointegration
+    
+
+
+
+
+
+    //Building a string command to read the file
+    const char * py_read1 = "df = pd.read_csv('";
+    const char * py_read2 = "')\n";
+    char buf[1024];
+    snprintf(buf, sizeof(buf), "%s%s%s", py_read1, argv[1], py_read2);
+    const char* str = buf;
+
+    const char * py_read3 = "df2 = pd.read_csv('";
+    char buf2[1024];
+    snprintf(buf2, sizeof(buf2), "%s%s%s", py_read3, argv[2], py_read2);
+    const char* str2 = buf2;
+
+    //Plotting the graph
+    PyRun_SimpleString(str);
+    PyRun_SimpleString(str2);
+    PyRun_SimpleString("plt.plot(df['Date'], df['Adj Close'], label='a')");
+    PyRun_SimpleString("plt.plot(df2['Date'], df2['Adj Close'], label='b')");
+    PyRun_SimpleString("plt.legend(loc='upper right')"); 
+    PyRun_SimpleString("plt.show()\n");
+    Py_Finalize();
 
     return 0;
 }
